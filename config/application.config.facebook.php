8b1ce068b201885d60c0d9fa178e11413c191a89
Bruno A. Hoffmann <bruno.a.hoffmann@gmail.com>
2017-09-17 22:28:14 -0300

    Cadastro e Login com Facebook

--- config/application.config.facebook.php
+++ config/application.config.facebook.php
@@ -2,6 +2,6 @@
 
 // Configura Host para envio de email
 return array(
-	'app_id' 	 => '175338859690479',
-	'app_secret' => '00935a09ed517d55c8f3b5b330be4da1'
+	'app_id' 	 => '',
+	'app_secret' => ''
 );
